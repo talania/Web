7865b798d4315a19f29982ac25ea28b11c89ebcf
talania <59729011+talania@users.noreply.github.com>
2020-04-28 17:54:55 +0400

    Add files via upload

--- Snake/script.js
+++ Snake/script.js
@@ -1,24 +1,43 @@
 let rect = document.getElementById('rect');
-let left = 0;
-let top = 0;
+let board = document.getElementById('board');
+let PosX = 0;
+let PosY = 0;
 
 function moveSnake(elem) {
-    if(elem.keyCode == 38) {
-        top += 2;
-        rect.style.top = top + 'px';
-    }
+    if(!rect) rect = document.getElementById('rect');
+    if(!board) board = document.getElementById('board');
     if(elem.keyCode == 40) {
-        top -= 2;
-        rect.style.top = top + 'px';
+        PosY += 10;
+        rect.style.top = PosY + 'px';
+        if(PosY >= board.offsetHeight-20){
+            PosY = 0-10;
+            rect.style.top = PosY;
+        }
     }
-    if(elem.keyCode == 37) {
-        left += 2;
-        rect.style.left = left + 'px';
+    if(elem.keyCode == 38) {
+        PosY -= 10;
+        rect.style.top = PosY + 'px';
+        if(PosY >= 0){
+            PosY = board.offsetHeight-20;
+            rect.style.top = PosY;
+        }
     }
     if(elem.keyCode == 39) {
-        left -= 2;
-        rect.style.left = left + 'px';
+        PosX += 10;
+        rect.style.left = PosX + 'px';
+        if(PosX >= board.offsetWidth-20){
+            PosX = 0-10;
+            rect.style.left = PosX;
+        }
+    }
+    if(elem.keyCode == 37) {
+        PosX -= 10;
+        rect.style.left = PosX + 'px';
+        if(PosX >= 0){
+            PosX = board.offsetWidth-20;
+            rect.style.left = PosX;
+        }
     }
 }
 
-document.onkeydown = movesnake();
+document.addEventListener('keydown',moveSnake);
